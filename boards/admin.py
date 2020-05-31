874a8aa22129c82ffdd0ec32c852a12f27cec7f9
Harshit Yadav <harshityadav95@gmail.com>
2020-05-31 13:35:59 +0530

    DB Update

--- boards/admin.py
+++ boards/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Board
+from .models import Topic
+from .models import Post
+from .models import ComapnySchedule
+from .models import CompanyCriteriaAndOffer
+from .models import CompanyTable
+
 
 admin.site.register(Board)
+admin.site.register(Topic)
+admin.site.register(Post)
+admin.site.register(CompanyTable)
+admin.site.register(ComapnySchedule)
+admin.site.register(CompanyCriteriaAndOffer)
+
